f01203d23d580b33bc5b347eb69e4dd9211d4ca8
ljh01067 <leejh01067@gmail.com>
2024-10-30 15:36:41 +0900

    스프링 시큐리티 설정(5강)부터 응답의 규격화(9강) 까지

--- src/main/java/com/example/rest_24_10/Rest2410Application.java
+++ src/main/java/com/example/rest_24_10/Rest2410Application.java
@@ -1,13 +1,13 @@
-package com.koreait.exam.rest_24_10;
+package com.example.rest_24_10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Rest2410Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Rest2410Application.class, args);
     }
-
 }
---
+++ src/main/java/com/example/rest_24_10/boundedContext/member/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.example.rest_24_10.boundedContext.member.service;
+
+import com.example.rest_24_10.base.jwt.JwtProvider;
+import com.example.rest_24_10.boundedContext.member.entity.Member;
+import com.example.rest_24_10.boundedContext.member.memberRepository.MemberRepository;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+
+    public Member join(String username, String password, String email) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+        memberRepository.save(member);
+        return member;
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public String genAccessToken(@NotBlank String username, @NotBlank String password) {
+        Member member = findByUsername(username).orElse(null);
+
+        if (member == null) return null;
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            return null;
+        }
+
+        return jwtProvider.genToken(member.toClaims(), 60 * 60 * 24 * 365);
+    }
+}
