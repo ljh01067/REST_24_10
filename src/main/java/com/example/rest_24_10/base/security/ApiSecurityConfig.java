896fc1a6cde56a72de1c1803265245304dba7a91
ljh01067 <leejh01067@gmail.com>
2024-11-01 15:26:37 +0900

    REST API 마무리

---
+++ src/main/java/com/example/rest_24_10/base/security/ApiSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.rest_24_10.base.security;
+
+import com.example.rest_24_10.base.security.entryPoint.ApiAuthenticationEntryPoint;
+import com.example.rest_24_10.base.security.filter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final ApiAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**") // 아래의 모든 설정은 /api/** 경로에만 적용
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                )
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests
+                                .requestMatchers("/api/*/member/login").permitAll() // 로그인은 누구나 가능
+                                .requestMatchers("/api/*/articles").permitAll() // 글 보기는 누구나 가능
+                                .anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
+                )
+                .cors().disable() // 타 도메인에서 API 호출 가능
+                .csrf().disable() // CSRF 토큰 끄기
+                .httpBasic().disable() // httpBasic 로그인 방식 끄기
+                .formLogin().disable() // 폼 로그인 방식 끄기
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(STATELESS)
+                ) // 세션 끄기
+                .addFilterBefore(
+                        jwtAuthorizationFilter, // 액세스 토큰으로부터 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
+    }
+}
